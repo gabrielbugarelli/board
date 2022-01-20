a4a8e09ac68f7d983a398724c9dd03203e927d4a
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2022-01-20 11:36:53 -0400

    feat: criação da Home.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
@@ -7,9 +8,21 @@ export default function Home() {
         Página inicial
       </Head>
 
-      <div>
-        <h1>Página inicial</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>Uma ferramenta para o seu dia a dia. Escreva, planeje e organize-se...</h1>
+
+          <p>
+            <span>100% Gratuita</span> e online.
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img src="https://avatars.githubusercontent.com/u/47955200?v=4" alt="avatar" />
+        </div>
+      </main>
     </>
   )
 }
