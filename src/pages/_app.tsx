9dd5e3778f4a27f75c51e9ec20dc307dc3188264
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-02-05 19:02:29 -0400

    feat: integração com a API de pagamento do Paypal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from 'next-auth/client';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 import '../styles/global.scss';
 
-import { Provider as NextAuthProvider } from 'next-auth/client';
+const initialProvider = {
+  'client-id': 'AfGHPA5hrAKcF5umUcPQ-Wg7C-oFD847UZfh7wkQ2Xy8Pj-7FBhrguD-rFnUdwFXS3ld3hZIwDT2UGi-',
+  currency: 'BRL',
+  intent: 'capture'
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session} >
-      <Header/>
-      <Component {...pageProps} />
+      <PayPalScriptProvider options={initialProvider}>
+        <Header/>
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </NextAuthProvider>
   )
 }
--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,7 +1,11 @@
 import styles from './styles.module.scss';
 import Head from 'next/head';
+import firebase from '../../services/firebaseConnection';
+
+import { PayPalButtons, OnApproveBraintreeActions, CreateOrderBraintreeActions } from '@paypal/react-paypal-js' 
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
+import { useState } from 'react';
 
 type DonateProps = {
   user: {
@@ -13,6 +17,48 @@ type DonateProps = {
 
 const Donate = ({ user }: DonateProps) => {
 
+  const [ userVip, setUserVip ] = useState<boolean>(false);
+
+  const handleSaveDonante = async () => {
+    await firebase.firestore().collection('users')
+    .doc(user.id)
+    .set({
+      donate: true,
+      lastDonate: new Date(),
+      image: user.image,
+    })
+
+    .then(() => {
+      setUserVip(true);
+    })
+  }
+
+  /**
+   * @description método responsável por efetuar o 
+   * pagamento da plataforma.
+   */
+  const onCreateOrderPaypal = async (data: Record<string, unknown>, actions: CreateOrderBraintreeActions) => {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '5'
+        }
+      }],
+    })
+  }
+
+  /**
+   * @description método responsável por
+   * validar o pagamento via Paypal
+   */
+  const onApproveOrderPaypal = async (data: Record<string, unknown>, actions: OnApproveBraintreeActions) => {
+    return actions.order.capture().then((details) => {
+      console.log('Compra aprovada:' + details.payer.name.given_name);
+
+      handleSaveDonante()
+    })
+  }
+
   return (
     <>
       <Head>
@@ -20,18 +66,25 @@ const Donate = ({ user }: DonateProps) => {
       </Head>
       
       <main className={styles.container}>
-        <img src="/images/rocket.svg" alt="Seja um apoiador e colabore com o projeto Taskdo" />
+        <img src="/images/rocket.svg" alt="Seja um apoiador e colabore com o projeto Taskido" />
 
-        <div className={styles.vip}>
-          <img src={user.image} alt="Foto de perfil do usuário" />
-          <span> Parabéns {user.name}, você apoiou o projeto Taskdo! 😄🎉</span>
-        </div>
+        {userVip && (
+          <div className={styles.vip}>
+            <img src={user.image} alt="Foto de perfil do usuário" />
+            <span> Obrigado {user.name}, você apoiou o projeto Taskido! 😄🎉</span>
+          </div>
+        )}
 
         <h1>
-          Seja um apoiador e colabore com o projeto Taskdo 🏆
+          Seja um apoiador e colabore com o projeto Taskido 🏆
         </h1>
 
         <strong>Apareça na nossa página inicial e obtenha funcionalidades exclusivas.</strong>
+
+        <PayPalButtons 
+          createOrder={(data, actions: CreateOrderBraintreeActions) => onCreateOrderPaypal(data, actions)} 
+          onApprove={(data, actions: OnApproveBraintreeActions) => onApproveOrderPaypal(data, actions)}  
+        />
       </main>
     </>
   )
